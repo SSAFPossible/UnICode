5d89a6d1030d3e1cd3981bf0161c8963e30141a0
김설희 <2750seolhee@naver.com>
2022-12-01 21:19:28 +0900

    📝 UPDATE : change word of DB ('mydb' -> 'UnICode')

--- src/main/resources/DB/UnICode.sql
+++ src/main/resources/DB/UnICode.sql
@@ -5,19 +5,19 @@ SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema UnICode
 -- -----------------------------------------------------
 
 -- -----------------------------------------------------
--- Schema mydb
+-- Schema UnICode
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 ;
-USE `mydb` ;
+CREATE SCHEMA IF NOT EXISTS `UnICode` DEFAULT CHARACTER SET utf8 ;
+USE `UnICode` ;
 
 -- -----------------------------------------------------
--- Table `mydb`.`User`
+-- Table `UnICode`.`User`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`User` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`User` (
   `uid` VARCHAR(45) NOT NULL,
   `password` VARCHAR(45) NULL,
   `name` VARCHAR(45) NULL,
@@ -34,9 +34,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Board`
+-- Table `UnICode`.`Board`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Board` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`Board` (
   `bid` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `content` VARCHAR(1000) NULL,
@@ -52,16 +52,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`Board` (
   INDEX `USER_BOARD_id_wirtre_idx` (`uid` ASC) VISIBLE,
   CONSTRAINT `USER_BOARD_ID_WRITER`
     FOREIGN KEY (`uid`)
-    REFERENCES `mydb`.`User` (`uid`)
+    REFERENCES `UnICode`.`User` (`uid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Project`
+-- Table `UnICode`.`Project`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Project` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`Project` (
   `pid` INT NOT NULL AUTO_INCREMENT,
   `title` VARCHAR(45) NOT NULL,
   `content` VARCHAR(1000) NULL,
@@ -76,9 +76,9 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`language`
+-- Table `UnICode`.`language`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`language` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`language` (
   `lid` INT NOT NULL AUTO_INCREMENT,
   `name` VARCHAR(45) NOT NULL,
   PRIMARY KEY (`lid`))
@@ -86,30 +86,30 @@ ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`B_Develop`
+-- Table `UnICode`.`B_Develop`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`B_Develop` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`B_Develop` (
   `bid` INT NOT NULL,
   `lid` INT NOT NULL,
   PRIMARY KEY (`bid`, `lid`),
   INDEX `LANGUAGE_DEVELOP_LID_idx` (`lid` ASC) VISIBLE,
   CONSTRAINT `BOARD_DEVELOP_BID`
     FOREIGN KEY (`bid`)
-    REFERENCES `mydb`.`Board` (`bid`)
+    REFERENCES `UnICode`.`Board` (`bid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `LANGUAGE_DEVELOP_LID`
     FOREIGN KEY (`lid`)
-    REFERENCES `mydb`.`language` (`lid`)
+    REFERENCES `UnICode`.`language` (`lid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`comment`
+-- Table `UnICode`.`comment`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`comment` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`comment` (
   `cid` INT NOT NULL AUTO_INCREMENT,
   `comment` VARCHAR(45) NULL,
   `bid` INT NULL,
@@ -123,42 +123,42 @@ CREATE TABLE IF NOT EXISTS `mydb`.`comment` (
   INDEX `USER_COMMENT_ID_idx` (`id` ASC) VISIBLE,
   CONSTRAINT `GROUP_NUM_CID`
     FOREIGN KEY (`parent_cid`)
-    REFERENCES `mydb`.`comment` (`cid`)
+    REFERENCES `UnICode`.`comment` (`cid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `BOARD_COMMENT_BID`
     FOREIGN KEY (`bid`)
-    REFERENCES `mydb`.`Board` (`bid`)
+    REFERENCES `UnICode`.`Board` (`bid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `USER_COMMENT_ID`
     FOREIGN KEY (`id`)
-    REFERENCES `mydb`.`User` (`uid`)
+    REFERENCES `UnICode`.`User` (`uid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`Board_Category`
+-- Table `UnICode`.`Board_Category`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`Board_Category` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`Board_Category` (
   `bid` INT NOT NULL,
   `main_class` INT NULL,
   `middle_class` INT NULL,
   PRIMARY KEY (`bid`),
   CONSTRAINT `BOARD_CATEGORY_BID`
     FOREIGN KEY (`bid`)
-    REFERENCES `mydb`.`Board` (`bid`)
+    REFERENCES `UnICode`.`Board` (`bid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`P_Develop`
+-- Table `UnICode`.`P_Develop`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`P_Develop` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`P_Develop` (
   `pid` INT NOT NULL,
   `lid` INT NOT NULL,
   PRIMARY KEY (`lid`, `pid`),
@@ -166,21 +166,21 @@ CREATE TABLE IF NOT EXISTS `mydb`.`P_Develop` (
   INDEX `PROJECT_DEVELOP_PID_idx` (`pid` ASC) VISIBLE,
   CONSTRAINT `PROJECT_DEVELOP_PID`
     FOREIGN KEY (`pid`)
-    REFERENCES `mydb`.`Project` (`pid`)
+    REFERENCES `UnICode`.`Project` (`pid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `LANGUAGE_DEVELOP_LID0`
     FOREIGN KEY (`lid`)
-    REFERENCES `mydb`.`language` (`lid`)
+    REFERENCES `UnICode`.`language` (`lid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`file_info`
+-- Table `UnICode`.`file_info`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`file_info` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`file_info` (
   `fid` INT NOT NULL AUTO_INCREMENT,
   `bid` INT NULL,
   `save_folder` VARCHAR(45) NULL,
@@ -190,16 +190,16 @@ CREATE TABLE IF NOT EXISTS `mydb`.`file_info` (
   INDEX `FILE_BOARD_BID_idx` (`bid` ASC) VISIBLE,
   CONSTRAINT `FILE_BOARD_BID`
     FOREIGN KEY (`bid`)
-    REFERENCES `mydb`.`Board` (`bid`)
+    REFERENCES `UnICode`.`Board` (`bid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
--- Table `mydb`.`ProjectMember`
+-- Table `UnICode`.`ProjectMember`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`ProjectMember` (
+CREATE TABLE IF NOT EXISTS `UnICode`.`ProjectMember` (
   `uid` VARCHAR(45) NOT NULL,
   `pid` INT NOT NULL,
   PRIMARY KEY (`uid`, `pid`),
@@ -207,28 +207,28 @@ CREATE TABLE IF NOT EXISTS `mydb`.`ProjectMember` (
   INDEX `fk_ProjectMember_Project1_idx` (`pid` ASC) VISIBLE,
   CONSTRAINT `fk_ProjectMember_User1`
     FOREIGN KEY (`uid`)
-    REFERENCES `mydb`.`User` (`uid`)
+    REFERENCES `UnICode`.`User` (`uid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT `fk_ProjectMember_Project1`
     FOREIGN KEY (`pid`)
-    REFERENCES `mydb`.`Project` (`pid`)
+    REFERENCES `UnICode`.`Project` (`pid`)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
-USE `mydb` ;
+USE `UnICode` ;
 
 -- -----------------------------------------------------
--- Placeholder table for view `mydb`.`view1`
+-- Placeholder table for view `UnICode`.`view1`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `mydb`.`view1` (`id` INT);
+CREATE TABLE IF NOT EXISTS `UnICode`.`view1` (`id` INT);
 
 -- -----------------------------------------------------
--- View `mydb`.`view1`
+-- View `UnICode`.`view1`
 -- -----------------------------------------------------
-DROP TABLE IF EXISTS `mydb`.`view1`;
-USE `mydb`;
+DROP TABLE IF EXISTS `UnICode`.`view1`;
+USE `UnICode`;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
