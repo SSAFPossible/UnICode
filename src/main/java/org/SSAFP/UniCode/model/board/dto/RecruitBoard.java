dcbb057afb0822dde8090380675df7a3ea0b6446
김설희 <2750seolhee@naver.com>
2022-12-07 17:03:06 +0900

    📝 UPDATE : #9 make mapper and update dto

--- src/main/java/org/SSAFP/UniCode/model/board/dto/RecruitBoard.java
+++ src/main/java/org/SSAFP/UniCode/model/board/dto/RecruitBoard.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class RecruitBoard extends Board {
-	private String mainImgUrl;		// 프로젝트 메인 이미지
 	private boolean open;			// 인원 모집 중인지 마감되었는지 여부
 	private int maxMember;			// 최대 인원
 	private int like;				// 좋아요
