d806b775b23ccf80af37d295027de83812f0730d
angly97 <angly97@naver.com>
2022-12-13 15:23:46 +0900

    📝 UPDATE : #6 add score handling

--- src/main/java/org/SSAFP/UniCode/model/board/service/StudyBoardServiceImpl.java
+++ src/main/java/org/SSAFP/UniCode/model/board/service/StudyBoardServiceImpl.java
@@ -1,11 +1,15 @@
 package org.SSAFP.UniCode.model.board.service;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.SSAFP.UniCode.model.board.dto.StudyBoard;
 import org.SSAFP.UniCode.model.board.dto.StudyBoardParam;
 import org.SSAFP.UniCode.model.board.repo.BoardRepo;
 import org.SSAFP.UniCode.model.board.repo.StudyBoardRepo;
+import org.SSAFP.UniCode.model.user.repo.UserRepo;
+import org.SSAFP.UniCode.score.Score;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -18,6 +22,9 @@ public class StudyBoardServiceImpl extends BoardServiceImpl {
 	
 	@Autowired
 	private BoardRepo boardRepo;
+	
+	@Autowired
+	private UserRepo userRepo;
 	
 	// Category와 tag 없이 모든 studyBoard 가져올 경우
 	@Transactional
@@ -42,8 +49,12 @@ public class StudyBoardServiceImpl extends BoardServiceImpl {
 		}
 		if (!write) {
 			throw new Exception();
+		} else { // 글 작성 성공 시 점수 증가 
+			Map<String, Object> scoreInfo = new HashMap<>();
+			scoreInfo.put("uid", studyBoard.getUid());
+			scoreInfo.put("score", Score.BOARD_WRITE);
+			write = userRepo.updateScore(scoreInfo);
+			return true;
 		}
-		return true;
-		
 	}
 }
