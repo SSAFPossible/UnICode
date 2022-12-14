3a13a44e330fea7176a400c42069cafbc677c7fa
김설희 <2750seolhee@naver.com>
2022-12-14 11:04:18 +0900

    ⭐ FEAT : #13 algorithm CRUD API

---
+++ src/main/java/org/SSAFP/UniCode/model/algorithm/dto/AlgoParam.java
@@ -0,0 +1,14 @@
+package org.SSAFP.UniCode.model.algorithm.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class AlgoParam {
+	private int solved;
+}
