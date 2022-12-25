d1004620b08dc97270fffd1af170bbcda2a95a22
SeolHee <59721896+seolhee2750@users.noreply.github.com>
2022-12-25 19:08:53 +0900

    🛠 FIX : #16 fix cicd error (related to docker run)

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ REPOSITORY=/home/ec2-user/ssafpossible/unicode
 
 cd $REPOSITORY
 
-docker stop UnICode
-docker rm UnICode
-docker rmi UnICode
+docker stop unicode
+docker rm unicode
+docker rmi unicode
 
 docker build -t unicode .
 docker run -d -p 8080:8080 --name unicode unicode
