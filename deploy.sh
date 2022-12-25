6d471ad186376aae0aaf690e8b8688bf2a5420d4
SeolHee <59721896+seolhee2750@users.noreply.github.com>
2022-12-25 18:11:37 +0900

    🛠 FIX : #16 set config

--- deploy.sh
+++ deploy.sh
@@ -4,9 +4,9 @@ REPOSITORY=/home/ec2-user/SSAFPossible/UnICode
 
 cd $REPOSITORY
 
-docker stop UnICode
-docker rm UnICode
-docker rmi UnICode
+# docker stop UnICode
+# docker rm UnICode
+# docker rmi UnICode
 
 docker build -t UnICode .
 docker run -d -p 8080:8080 —name UnICode UnICode
